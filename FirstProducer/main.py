6d420ebd5ed152d2b1a92d02abf957e945ecdd60
Meir <meirroc5764@gmail.com>
2026-09-23 10:05:16 +0300

    add a producer and jupiter

---
+++ FirstProducer/main.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from confluent_kafka import Producer
+
+config = {"bootstrap.servers" : "localhost:9092"}
+
+producer = Producer(config)
+
+file_name = "activity_readings.csv"
+
+path = next(Path("c:/").rglob(file_name))
+print(path)
+
+with open(path, "r", encoding="utf-8") as f:
+    all_data = f.readlines()
+
+count = 0
+for data in all_data:
+
+    try:
+
+        producer.produce(data)
+        count += 1
+        print("send to kafka")
+
+    except Exception as e:
+        print(f"you have {e.args} exepsion")
+
+print(f"send {count} message to kafka")
+
